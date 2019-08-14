34df3d97abb98f6ee34b250a2ff06544e24c502c
leegeunhyeok <dev.ghlee@gmail.com>
2019-08-15 03:46:18 +0900

    UPDATE: Remove sudo keyword

--- elk.sh
+++ elk.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# Checking permission
-sudo -n true
-if [ $? -ne 0 ]
-    then
-        echo "Check your permission"
-        exit
-fi
-
 dependency_check_deb() {
 java -version
 if [ $? -ne 0 ]
     then
-        sudo apt-get install openjdk-8-jre-headless -y
+        apt-get install openjdk-8-jre-headless -y
     elif [ "`java -version 2> /tmp/version && awk '/version/ { gsub(/"/, "", $NF); print ( $NF < 1.8 ) ? "YES" : "NO" }' /tmp/version`" == "YES" ]
         then
-            sudo apt-get install openjdk-8-jre-headless -y
+            apt-get install openjdk-8-jre-headless -y
 fi
 }
 
@@ -23,44 +15,44 @@ dependency_check_rpm() {
     java -version
     if [ $? -ne 0 ]
         then
-            sudo yum install jre-1.8.0-openjdk -y
+            yum install jre-1.8.0-openjdk -y
         elif [ "`java -version 2> /tmp/version && awk '/version/ { gsub(/"/, "", $NF); print ( $NF < 1.8 ) ? "YES" : "NO" }' /tmp/version`" == "YES" ]
             then
-                sudo yum install jre-1.8.0-openjdk -y
+                yum install jre-1.8.0-openjdk -y
     fi
 }
 
 debian_elk() {
-    sudo apt-get update
-    sudo wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/logstash/logstash-6.0.0-rc2.deb
-    sudo dpkg -i /opt/logstash-6.0.0-rc2.deb
-    sudo wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0-rc2.deb
-    sudo dpkg -i /opt/elasticsearch-6.0.0-rc2.deb
-    sudo apt-get install apt-transport-https
-    sudo wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/kibana/kibana-6.0.0-rc2-amd64.deb
-    sudo dpkg -i /opt/kibana-6.0.0-rc2-amd64.deb
+    apt-get update
+    wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/logstash/logstash-6.0.0-rc2.deb
+    dpkg -i /opt/logstash-6.0.0-rc2.deb
+    wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0-rc2.deb
+    dpkg -i /opt/elasticsearch-6.0.0-rc2.deb
+    apt-get install apt-transport-https
+    wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/kibana/kibana-6.0.0-rc2-amd64.deb
+    dpkg -i /opt/kibana-6.0.0-rc2-amd64.deb
 
-    sudo systemctl restart logstash
-    sudo systemctl enable logstash
-    sudo systemctl restart elasticsearch
-    sudo systemctl enable elasticsearch
-    sudo systemctl restart kibana
-    sudo systemctl enable kibana
+    systemctl restart logstash
+    systemctl enable logstash
+    systemctl restart elasticsearch
+    systemctl enable elasticsearch
+    systemctl restart kibana
+    systemctl enable kibana
 }
 
 rpm_elk() {
-    sudo yum install wget -y
-    sudo wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/logstash/logstash-6.0.0-rc2.rpm
-    sudo rpm -ivh /opt/logstash-6.0.0-rc2.rpm
-    sudo wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0-rc2.rpm
-    sudo rpm -ivh /opt/elasticsearch-6.0.0-rc2.rpm
-    sudo wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/kibana/kibana-6.0.0-rc2-linux-x86_64.tar.gz
-    sudo tar zxf /opt/kibana-6.0.0-rc2-linux-x86_64.tar.gz -C /opt/
+    yum install wget -y
+    wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/logstash/logstash-6.0.0-rc2.rpm
+    rpm -ivh /opt/logstash-6.0.0-rc2.rpm
+    wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0-rc2.rpm
+    rpm -ivh /opt/elasticsearch-6.0.0-rc2.rpm
+    wget --directory-prefix=/opt/ https://artifacts.elastic.co/downloads/kibana/kibana-6.0.0-rc2-linux-x86_64.tar.gz
+    tar zxf /opt/kibana-6.0.0-rc2-linux-x86_64.tar.gz -C /opt/
 
     # Starting Services
-    sudo service logstash start
-    sudo service elasticsearch start
-    sudo /opt/kibana-6.0.0-rc2-linux-x86_64/bin/kibana &
+    service logstash start
+    service elasticsearch start
+    /opt/kibana-6.0.0-rc2-linux-x86_64/bin/kibana &
 }
 
 # Install ELK Stack
